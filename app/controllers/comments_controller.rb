a8737aab7fc6dfcedb451aa2caea1b8c97c22e8a
Corey Rugino <coreyrugino@gmail.com>
2015-12-07 22:58:50 -0700

    working, linging from edit page to users

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,52 @@
+class CommentsController < ApplicationController
+
+  def index
+    @comments = Comment.all
+  end
+
+  def show
+
+    @comment = Comment.find(params[:id])
+
+  end
+
+  def new
+    @comments = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to post_path(@comment.post_id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :post_id)
+  end
+
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+  end
+
+  def new
+
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to user_path(@post.user_id)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :user_id)
+  end
+
+
+end
